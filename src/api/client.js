437565f56f3f5cf4cfff5cafd2d010061777ba9a
ricardo.freitas <ricardo.freitas@maxinst.com>
2026-09-12 00:13:48 -0300

    Ajuste de endpoint do backend

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  //baseURL: 'http://localhost:3001',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001',
   withCredentials: true, // importante para manter a sessão
   headers: { 'Content-Type': 'application/json' },
 });
