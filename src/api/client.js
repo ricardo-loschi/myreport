f34a4f7887c793bd82b411cfb473e1850f9faf16
ricardo.freitas <ricardo.freitas@maxinst.com>
2026-09-13 22:27:56 -0300

    Ajuste api url

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:3001',
-  baseURL: process.env.APP_API_URL || 'http://localhost:3001',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001',
   withCredentials: true, // importante para manter a sessão
   headers: { 'Content-Type': 'application/json' },
 });
